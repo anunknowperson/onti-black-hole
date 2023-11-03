8f07f9003246d3b8488bc0f8deffafa2065469c9
anunknowperson <kiselev.sereja@gmail.com>
2023-11-03 20:29:17 +0300

    Add project

---
+++ Solution.gd
@@ -0,0 +1,17 @@
+extends SolutionWrapper
+
+@onready
+var Stars := get_stars()
+
+@onready
+var MatterCSGBox := get_matter_csg_node()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Replace with function body.
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
